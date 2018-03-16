ee84997d662910e276157a557e4810761a4c6b7f
Clariza <clariza21@outlook.com>
2018-03-15 20:00:08 -0500

    App

--- src/components/Section.vue
+++ src/components/Section.vue
@@ -5,7 +5,7 @@
         div.container-info--app
           img.img1(src='https://domicilios.com.pe/images/placeholders/phones@2x.png')
         div.container-info--description
-          h1.title Nuestra App
+          h1.title NUESTRA APP
           p.parrafo Pide tu comida preferida en el momento que desees desde nuestra aplicacion móvil para iOS y Android. Contamos con más de 4000 restaurantes por lo que encontrarás una oferta inigualable. ¡Descárgala ahora! Es gratis y podrás disfrutar de tus platos favoritos.
 
 
